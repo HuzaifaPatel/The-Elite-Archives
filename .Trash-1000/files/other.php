14de6e36dbf1f1318e8e42fa15f0594e79e46e9d
Huzi <43390871+HuzaifaPatel@users.noreply.github.com>
2023-05-15 17:25:59 -0400

    remove .Trash-1000

--- .Trash-1000/files/other.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once 'config/init.php';
-
-$other = new Template('templates/frontend/other.tpl');
-
-echo $other;
--- .Trash-1000/files/site-updates.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once 'config/init.php';
-
-$update = new Template('templates/frontend/site-updates.tpl');
-
-echo $update;
